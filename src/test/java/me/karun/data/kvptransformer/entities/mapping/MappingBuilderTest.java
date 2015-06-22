0f97bbd85416c3ab2179cd906d4568bd738e160b
Karun Japhet <karun@japhet.in>
2015-06-22 09:36:17 +0530

    Adding test to ensure multiple transformer chains work.

--- src/test/java/me/karun/data/kvptransformer/entities/mapping/MappingBuilderTest.java
+++ src/test/java/me/karun/data/kvptransformer/entities/mapping/MappingBuilderTest.java
@@ -57,12 +57,31 @@ public class MappingBuilderTest {
     final String source = "source";
     final String target = "target";
     final Function<String, Integer> transform = Integer::parseInt;
-    final Mapping actualMapping = new MappingBuilder(expectedMapping, source, target)
-      .withTransform(transform)
+    final Mapping actualMapping = new MappingBuilder(expectedMapping, source, target).withTransform(transform)
       .build();
 
     assertEquals(actualMapping, expectedMapping);
     assertOnMapping.accept(actualMapping, new Pair<>(source, target));
     assertOnTransform.accept(actualMapping, new Pair<>(source, transform));
+  }
+
+  @Test
+  public void shouldBuildMultipleTransformation() {
+    final Mapping expectedMapping = new Mapping();
+    final String source1 = "source1";
+    final String target1 = "target1";
+    final String source2 = "source2";
+    final String target2 = "target2";
+    final Function<String, Integer> transform1 = Integer::parseInt;
+    final Function<Integer, String> transform2 = String::valueOf;
+    final Mapping actualMapping = new MappingBuilder(expectedMapping, source1, target1).withTransform(transform1).and()
+      .source(source2).toTarget(target2).withTransform(transform2)
+      .build();
+
+    assertEquals(actualMapping, expectedMapping);
+    assertOnMapping.accept(actualMapping, new Pair<>(source1, target1));
+    assertOnTransform.accept(actualMapping, new Pair<>(source1, transform1));
+    assertOnMapping.accept(actualMapping, new Pair<>(source2, target2));
+    assertOnTransform.accept(actualMapping, new Pair<>(source2, transform2));
   }
 }
