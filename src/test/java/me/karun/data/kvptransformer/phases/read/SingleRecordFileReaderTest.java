638c0d6b33f4550f5e4b74cbaaa384a44742642b
Karun Japhet <karun@japhet.in>
2015-07-06 16:22:00 +0530

    Adding test to check invalid file names throw an exception

--- src/test/java/me/karun/data/kvptransformer/phases/read/SingleRecordFileReaderTest.java
+++ src/test/java/me/karun/data/kvptransformer/phases/read/SingleRecordFileReaderTest.java
@@ -2,6 +2,9 @@ package me.karun.data.kvptransformer.phases.read;
 
 import org.testng.annotations.Test;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.NoSuchFileException;
 import java.util.Arrays;
 import java.util.List;
 
@@ -10,12 +13,18 @@ import static org.testng.Assert.assertEquals;
 public class SingleRecordFileReaderTest {
 
   @Test
-  public void shouldReadFilesCorrectly() throws Exception {
+  public void shouldReadFilesCorrectly() throws IOException, URISyntaxException {
     final String fileName = "src/test/resources/single-record"; // FIXME should be paths relative to resources
     final List<String> expectedLines = Arrays.asList("key1=value1","key2=2");
 
     final List<String> actualLines = new SingleRecordFileReader(fileName).read();
 
     assertEquals(actualLines, expectedLines);
+  }
+
+  @Test(expectedExceptions = {NoSuchFileException.class})
+  public void shouldThrowErrorWhenFileDoesNotExist() throws IOException, URISyntaxException {
+    final String invalidFileName = "some-file-that-does-not-exist";
+    new SingleRecordFileReader(invalidFileName).read();
   }
 }
