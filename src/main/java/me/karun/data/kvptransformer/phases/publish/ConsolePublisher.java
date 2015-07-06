06f50a30afb5b55d50afc22793df0a57f37258fd
Karun Japhet <karun@japhet.in>
2015-07-06 17:11:14 +0530

    Adding a simple ConsolePublisher for messages

---
+++ src/main/java/me/karun/data/kvptransformer/phases/publish/ConsolePublisher.java
@@ -0,0 +1,22 @@
+package me.karun.data.kvptransformer.phases.publish;
+
+import me.karun.data.kvptransformer.entities.message.Message;
+
+import java.io.PrintStream;
+
+public class ConsolePublisher {
+
+  private final PrintStream stream;
+
+  public ConsolePublisher() {
+    this(System.out);
+  }
+
+  public ConsolePublisher(final PrintStream stream) {
+    this.stream = stream;
+  }
+
+  public void publish(final Message message) {
+    stream.println(message.toString());
+  }
+}
---
+++ src/test/java/me/karun/data/kvptransformer/phases/publish/ConsolePublisherTest.java
@@ -0,0 +1,25 @@
+package me.karun.data.kvptransformer.phases.publish;
+
+import me.karun.data.kvptransformer.entities.message.Message;
+import org.testng.annotations.Test;
+
+import java.io.PrintStream;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class ConsolePublisherTest {
+
+  @Test
+  public void testPublish() throws Exception {
+    final Message message = mock(Message.class);
+    final PrintStream stream = mock(PrintStream.class);
+    final ConsolePublisher publisher = new ConsolePublisher(stream);
+
+    when(message.toString()).thenReturn("{key1=1,key2=2}");
+    publisher.publish(message);
+
+    verify(stream).println("{key1=1,key2=2}");
+  }
+}
